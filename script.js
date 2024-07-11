99f2cdcac3a4224bc5330e5838256d2cec4c7a30
Local_Sholum <localSholum@gmail.com>
2024-07-11 12:27:07 +0700

    i think this last commit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ for (i = 0; i < myNodelist.length; i++) {               //buat looping dimana i 
 // buat fungsi agar closs button dapat berfungsi untuk menutup list item
 var close = document.getElementsByClassName("close");   //inisiasi variabel close dengan fungsi untuk mengambil elemen dari nama class close
 var i;                    //inisiasi variabel i
-for (i = 0; i < close.length; ni++) {           //buat perulangan untuk setiap kondisi close.lenght
+for (i = 0; i < close.length; i++) {            //buat perulangan untuk setiap kondisi close.lenght
   close[i].onclick = function() {               //disini merupakan event handler dimana setiap index dari close di click maka akan mengaktifkan function()    |untuk event handler bisa dilihat di google 
     var div = this.parentElement;               //inisiasi div dengan fungsi untuk mengambil parent elemen dari elemen yang di klik, disini elemen yang diambil yaitu elemen span
     div.style.display = "none";                 //kemudian membuat elemen li disembunyikan dengan style display none
@@ -21,10 +21,37 @@ for (i = 0; i < close.length; ni++) {           //buat perulangan untuk setiap k
 }
 
 //tambahkan simbol check ketika mengklik list item
-var list = document.querrySelector('ul');             //inisiasi list dengan fungsi untuk mengambil selector ul
-list.addEventListener('click', function(ev) { 
-  if (ev.target.tagName ===  'LI') {
-    ev.target.classList.toggle("checked");
+var list = document.querySelector('ul');              //inisiasi list dengan fungsi untuk mengambil selector ul
+list.addEventListener('click', function(ev) {         //tambahkan event listener click ke list, kemudian tambahkan fungsi callback saat event terjadi, disini (ev) mewakili event
+  if (ev.target.tagName ===  'LI') {                  //buat kondisi jika event yang ditargetkan tagname merupakan LI
+    ev.target.classList.toggle('checked');            //maka targetkan classList dengan tanda checked
   }
-}, false);
+}, false);                                            //jika kondisi tidak terpenuhi false
+
+// buat list item baru ketika tombol add ditekan
+function newElement() {                           //buat fungsi newElemen  
+  var li = document.createElement("li");          //inisiasi li dengan fungsi untuk membuat elemen li di dokumen
+  var inputValue = document.getElementById("myInput").value;   //inisiasi variabel dengan fungsi mengambil elemen dengan id "myInput di dokumen. kemudian nilai tersebut diambil dengan .value"
+  var t = document.createTextNode(inputValue);                //inisiasi variabel dengan fungsi untuk membuat text node dengan nilai dari variabel inputValue | value node text bisa dimasukkan kedalam html
+  li.appendChild(t);            //menyisipkan nodeText milik variabel t kedalam variabel li
+  if (inputValue === '') {      //buat percabangan dengan kondisi jika inputValue berisi value kosong
+    alert("You must add / write something");        //maka tambahkan alert / pemberitahuan
+  } else {                                          //sebaliknya
+    document.getElementById("myUL").appendChild(li);    //ambil elemen dengan id "myUL" kemudian tambahkan variabel/elemen li sebagai child dari elemen dengan id myUL
+  }
+  document.getElementById("myInput").value="";          //ambil elemen dengan id myInput. kemudian akses value,selanjutnya atur nilai value sebagai string kosong
+
+  var span = document.createElement("SPAN");            //inisiasi span dengan fungsi untuk menambahkan elemen SPAN
+  var txt = document.createTextNode("\u00D7");          //inisiasi txt dengan membuat textNode tanda tutup (x)
+  span.className = "close";                             //mengatur properti dengan className span dengan close
+  span.appendChild(txt);                //tambahkan child nodeTxt (txt) kedalam elemen span
+  li.appendChild(span);                 //tambahkan child elemen span kedalam parent li
+
+  for (i=0; i < close.length; i++) {      //buat perulangan dengan kondisi panjang close
+    close[i].onclick = function() {       //setiap index close tambahkan event handler onclick dengan fungsi
+      var div = this.parentElement;       //inisiasi div dengan mengambi parent elemen div
+      div.style.display = "none";         //atur style div dengan display none
+    }
+  }
+}
 
